040a8e85b3ce565d6dc54084ab6adee240f59bbe
Nora Hønnåshagen Hansen <nora.hans1@hotmail.com>
2024-01-19 09:08:18 +0100

    Refactored Computer class, tests now pass

--- src/main/java/com/booleanuk/core/Computer.java
+++ src/main/java/com/booleanuk/core/Computer.java
@@ -4,20 +4,31 @@ import java.util.ArrayList;
 
 public class Computer {
     public ArrayList<Game> installedGames = new ArrayList<>();
+    private final PowerSupply psu;
+
+    public Computer(PowerSupply psu)
+    {
+        this.psu = psu;
+    }
+
+    public Computer(PowerSupply psu, ArrayList<Game> preinstalledGames)
+    {
+        this.psu = psu;
+        this.installedGames = preinstalledGames;
+    }
 
     public void turnOn() {
-        PowerSupply psu = new PowerSupply();
         psu.turnOn();
     }
 
-    public void installGame() {
-        Game game = new Game("Morrowind");
+    public void installGame(String gameTitle) {
+        Game game = new Game(gameTitle);
         this.installedGames.add(game);
     }
 
-    public String playGame() {
+    public String playGame(String gameTitle) {
         for (Game g : this.installedGames) {
-            if (g.name.equals("Morrowind")) {
+            if (g.name.equals(gameTitle)) {
                 return g.start();
             }
         }
